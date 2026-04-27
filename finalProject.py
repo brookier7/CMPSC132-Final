f40d76a052263efd933185db9f951cb1929ac528
brookier7 <andrewbbrook@icloud.com>
2026-04-27 17:36:10 -0400

    Main logic implemented

--- finalProject.py
+++ finalProject.py
@@ -5,12 +5,28 @@ April 28, 2026
 Final Project
 """
 
-def make_guess(num, correct):
+import random
+
+
+def make_guess(guess, correct):
     """
     Inputs a numerical guess
     Returns a result based on if the number is too high/low/correct
     """
-    pass
+    if guess > correct:
+        return "Too high"
+    elif guess < correct:
+        return "Too low"
+    else:
+        return "Correct!"
 
 if __name__ == "__main__":
-    pass
+    correct = random.randint(1,100)
+    num_guesses = 1
+    guess = int(input("Guess a number: "))
+    while make_guess(guess,correct)!="Correct!":
+        print(make_guess(guess,correct))
+        num_guesses+=1
+        guess = int(input("Guess a number: "))
+    print("Correct!\nCongratulations!\nIt took you "+str(num_guesses)+" guesses!")
+
