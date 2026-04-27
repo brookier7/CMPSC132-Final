6bdd9d7a4628c41294c004e960c8a8afc87a4659
brookier7 <andrewbbrook@icloud.com>
2026-04-27 16:59:58 -0400

    Created methods

--- finalProject.py
+++ finalProject.py
@@ -1,1 +1,16 @@
+"""
+Andrew Brook
+CMPSC 132
+April 28, 2026
+Final Project
+"""
 
+def make_guess(num, correct):
+    """
+    Inputs a numerical guess
+    Returns a result based on if the number is too high/low/correct
+    """
+    pass
+
+if __name__ == "__main__":
+    pass
